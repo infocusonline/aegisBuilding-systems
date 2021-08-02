17179e4c6bd38e064b41c1621b1b3de8a89278eb
Miguel Tejeda <tejedamiguel6@aol.com>
2021-08-02 10:59:12 -0400

    fixed pixels

--- src/components/PageHero/PageHero.styles.js
+++ src/components/PageHero/PageHero.styles.js
@@ -2,7 +2,7 @@ import styled from 'styled-components'
 import { GatsbyImage } from 'gatsby-plugin-image'
 
 export const StyledImg = styled(GatsbyImage)`
-  height: 420px;
+  height: 435px;
   padding: 20px;
   width: 100%;
 `
